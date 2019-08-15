e223a29674398eb129ea6622d56b23157cba7488
dj_rdm <vanadis0929@gmail.com>
2019-08-15 14:51:55 +0900

    작업 중

--- asset/js/common.js
+++ asset/js/common.js
@@ -1,3 +1,30 @@
-$(function () {
+//   const default = {
+//       //팝업에 있어서 기본 값 정의
 
+//       //열리는 데 필요한 것
+
+//       //생성하는 데 필요한 것
+
+//       //닫히는데 필요한 것
+//   }
+
+$(function() {
+  var init = function() {
+    console.log("target");
+
+    const popup = {
+      open: function(popid) {
+        let target = $("#layer_01");
+        target = $("#" + popid);
+        target.show();
+        console.log(target);
+      },
+
+      create: function(popid) {},
+
+      close: function(popid) {}
+    };
+  };
+
+  init();
 });
