65e2ff64f75292488daea91bb8c7b1fdd9a99e14
seungsoo.baek <leaims86@google.com>
2019-08-25 03:22:47 +0900

    prototype test

--- asset/js/jquery/common.js
+++ asset/js/jquery/common.js
@@ -8,59 +8,98 @@
 */
 
 $(function () {
-    var DimHtml = $('<div class="dim"></div>'); // dim 생성
-    const delayTime = 5;
+//     var DimHtml = $('<div class="dim"></div>'); // dim 생성
+//     const delayTime = 5;
 
-    /* 레이어팝업 열기 */
-    $('.layer-pop-btn').on("click", function(){
-        $('.layer-popup').addClass('on');
-        $('body').append(DimHtml);
-        timeOut(delayTime);
+//     /* 레이어팝업 열기 */
+//     $('.layer-pop-btn').on("click", function(){
+//         $('.layer_wrap').addClass('on');
+//         $('body').append(DimHtml);
+//         timeOut(delayTime);
         
-    });
-    /* 레이어팝업 닫기 */
-    $('.popup-close-btn').on("click", function(){
-        $(this).parents('.layer-popup').removeClass('on');
-        DimHtml.remove();
-        timeOut(0);
-    });
+//     });
+//     /* 레이어팝업 닫기 */
+//     $('.popup-close-btn').on("click", function(){
+//         $(this).parents('.layer_wrap').removeClass('on');
+//         DimHtml.remove();
+//         timeOut(0);
+//     });
 
-    /* dim영역 닫기 */
-    $(document).on("click", ".dim" , function(){
-        $('.layer-popup').removeClass('on');
-        $(this).remove();
-        timeOut(0);
-    });
+//     /* dim영역 닫기 */
+//     $(document).on("click", ".dim" , function(){
+//         $('.layer_wrap').removeClass('on');
+//         $(this).remove();
+//         timeOut(0);
+//     });
 
-    /* 5초후 닫기 */
-    function timeOut(stop){
-        setTimeout(function(){
-            $('.layer-popup').removeClass('on');
-            DimHtml.remove();
-        },delayTime*1000);
-    }
-});
+//     /* 5초후 닫기 */
+//     function timeOut(stop){
+//         setTimeout(function(){
+//             $('.layer_wrap').removeClass('on');
+//             DimHtml.remove();
+//         },delayTime*1000);
+//     }
+
+    
 
 
     /* prototype... */
     function layerPop() {
 
+        $('.btn_poplayer').on('click', this.openPop.bind(this)); // btn 클릭시 openPop 실행
+        $('.btn_close').on('click', this.closePop.bind(this)); // btn 클릭시 closePop 실행
+
         
+
     };
     
-    layerPop.prototype.create = function(){
+    layerPop.prototype.openPop = function(event){
+        console.log(this.dataset.popup);
+        var popData = $('.btn_poplayer').data('popup'); // prototype은 this가 다름.
+        console.log(popData);
+        if ( 'open' == popData ){
+            console.log('????')
+            $('.layer_wrap').addClass('on');
+            this.render();
+        };
+        if ('layer' == popData){
+            console.log('!!!!');
+            this.create();
+        }
+    }
+    layerPop.prototype.closePop = function(){
+        var popData = $('.btn_close').data('popup');
+        if ( 'close' == popData ){
+            $('.layer_wrap').removeClass('on');
+            this.delete();
+        };
+    }
 
-        this.render(); // dim호출
+
+
+    layerPop.prototype.create = function(){
+        var popUp =
+            $('<div class="layer_wrap on">' +
+                '  <h1>레이어팝업</h1>  ' +
+                '  <p>임시</p> ' +
+                '  <button type="button" class="btn_close" data-popup="close">닫기</button>' +
+                '</div>');
+        $('body').append(popUp);
     }
-    
+
+
     layerPop.prototype.render = function(){
-        let $body = $('body');
-        $body.append('<div class="dim"></div>');
+        $('body').append('<div class="dim"></div>');
     }
-    
+    layerPop.prototype.delete = function(){
+        $('.dim').remove();
+    }
+
+
     function startApp() {
         let app = new layerPop();
-      }
+    }
       
-      $(startApp);
+    $(startApp);
 
+ });
